bbc6e46bc965b529444f72b7661987ee0bf27bd2
Anonymous User <52798256+plocket@users.noreply.github.com>
2026-05-05 09:55:47 -0400

    Insert survey monkey embed script properly

---
+++ docassemble/BehaviorSnippets/data/static/insert_arbitrary_script.js
@@ -0,0 +1,27 @@
+/** Put this in your branding package/org framework. */
+$(document).on('daPageLoad', async function() {
+  /**
+   * Add a script to the page only when an appropriate. In this case, the script will
+   *     add a Survey Monkey iframe. You can abstract this more using window-level
+   *     variables.
+   * 
+   * Avoid layout shift by using an initial dummy element of the same size as the
+   * final element plus some other details: https://web.dev/articles/optimize-cls
+   */
+
+  const relocated_class = `relocated`;
+  const $script_parent = $(`#survey_monkey1`);
+
+  // Stops executing this code when 1. a specific element missing or
+  // 2. we have already done this.
+  if ( !$script_parent[0] || $script_parent.hasClass( relocated_class ) ) {
+    return;
+  }
+
+  // Add the script to the page
+  $script_parent.append( $survey_monkey_script );
+
+  // Storing the state of affairs on the page
+  $script_parent.addClass( relocated_class );
+  
+});
---
+++ docassemble/BehaviorSnippets/data/static/survey_monkey_script.js
@@ -0,0 +1,13 @@
+/** Put this in the individual interview. */
+/**
+ * Create the script element with the modified Survey Monkey
+ * code. Original script is at the bottom[^1]
+ * ⚠️ Avoid code that uses strings with code in them in this way
+ * in general as it tends to be insecure. Only let trusted editors
+ * edit this.
+ */
+const $survey_monkey_script = $(`<script>(function(t,e,s,n){var o,a,c;t.SMCX=t.SMCX||[],e.getElementById(n)||(o=[e.getElementById(s)],a=o[o.length-1],c=e.createElement('script'),c.type="text/javascript",c.async=!0,c.id=n,c.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd8qZ8ON_2FyjUqTzYvfSuztE4hyTngEkIeKEiezipnuyLG.js",a.parentNode.insertBefore(c,a))})(window,document,"survey_monkey1","smcx-sdk");</script>`);
+
+/**
+ * [^1]: The original Survey Monkey embed script: (function(t,e,s,n){var o,a,c;t.SMCX=t.SMCX||[],e.getElementById(n)||(o=e.getElementsByTagName(s),a=o[o.length-1],c=e.createElement(s),c.type="text/javascript",c.async=!0,c.id=n,c.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd8qZ8ON_2FyjUqTzYvfSuztE5sCoyqizjOaci83tTsBvPe.js",a.parentNode.insertBefore(c,a))})(window,document,"script","smcx-sdk");
+ */
