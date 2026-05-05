ed16f4d9e4c165eb674b897fda445a837890c823
Anonymous User <52798256+plocket@users.noreply.github.com>
2026-05-05 12:33:12 -0400

    Make Survey Monkey embed ids match to reduce confusion

--- docassemble/BehaviorSnippets/data/static/survey_monkey_script.js
+++ docassemble/BehaviorSnippets/data/static/survey_monkey_script.js
@@ -9,5 +9,5 @@
 const $survey_monkey_script = $(`<script>(function(t,e,s,n){var o,a,c;t.SMCX=t.SMCX||[],e.getElementById(n)||(o=[e.getElementById(s)],a=o[o.length-1],c=e.createElement('script'),c.type="text/javascript",c.async=!0,c.id=n,c.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd8qZ8ON_2FyjUqTzYvfSuztE4hyTngEkIeKEiezipnuyLG.js",a.parentNode.insertBefore(c,a))})(window,document,"survey_monkey1","smcx-sdk");</script>`);
 
 /**
- * [^1]: The original Survey Monkey embed script: (function(t,e,s,n){var o,a,c;t.SMCX=t.SMCX||[],e.getElementById(n)||(o=e.getElementsByTagName(s),a=o[o.length-1],c=e.createElement(s),c.type="text/javascript",c.async=!0,c.id=n,c.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd8qZ8ON_2FyjUqTzYvfSuztE5sCoyqizjOaci83tTsBvPe.js",a.parentNode.insertBefore(c,a))})(window,document,"script","smcx-sdk");
+ * [^1]: The original Survey Monkey embed script: (function(t,e,s,n){var o,a,c;t.SMCX=t.SMCX||[],e.getElementById(n)||(o=e.getElementsByTagName(s),a=o[o.length-1],c=e.createElement(s),c.type="text/javascript",c.async=!0,c.id=n,c.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd8qZ8ON_2FyjUqTzYvfSuztE4hyTngEkIeKEiezipnuyLG.js",a.parentNode.insertBefore(c,a))})(window,document,"script","smcx-sdk");
  */
